9bba2624ce101093416ae1fc9247a6a068e0269f
Mauro De Souza <maurodesouza2017@hotmail.com>
2020-01-30 23:30:19 -0300

    class 04 - config sequelize

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: 'postgress',
+  host: '192.168.99.100',
+  username: 'postgres',
+  password: 'apenasumshow',
+  database: 'gobarber',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
