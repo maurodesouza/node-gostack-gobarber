1ed7afc325fc1f4f082605c1e11823dac50b254e
Mauro De Souza <maurodesouza2017@hotmail.com>
2020-02-01 02:32:45 -0300

    class 10 - authentication middleware created

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -21,6 +21,10 @@ class UserController {
       return res.json({ error: 'Fail on create new user' });
     }
   }
+
+  async update(req, res) {
+    return res.json({ ok: true });
+  }
 }
 
 export default new UserController();
