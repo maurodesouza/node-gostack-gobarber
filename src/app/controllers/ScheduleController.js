de770e20cdf2b163d056626ab37265abdce5cc76
Mauro De Souza <maurodesouza2017@hotmail.com>
2020-02-04 23:49:57 -0300

    class 21 - listing the provider's agenda

---
+++ src/app/controllers/ScheduleController.js
@@ -0,0 +1,33 @@
+import { startOfDay, endOfDay, parseISO } from 'date-fns';
+import { Op } from 'sequelize';
+
+import User from '../models/User';
+import Appointment from '../models/Appointment';
+
+class ScheduleController {
+  async index(req, res) {
+    const isProvider = await User.findOne({
+      where: { id: req.userId, provider: true },
+    });
+
+    if (!isProvider)
+      return res.status(401).json({ error: 'User is not a provider' });
+
+    const parseDate = parseISO(req.query.date);
+
+    const appointment = await Appointment.findAll({
+      where: {
+        provider_id: req.userId,
+        canceled_at: null,
+        date: {
+          [Op.between]: [startOfDay(parseDate), endOfDay(parseDate)],
+        },
+      },
+      order: ['date'],
+    });
+
+    return res.json(appointment);
+  }
+}
+
+export default new ScheduleController();
--- src/routes.js
+++ src/routes.js
@@ -7,6 +7,7 @@ import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
 import AppointmentController from './app/controllers/AppointmentController';
+import ScheduleController from './app/controllers/ScheduleController';
 
 import authMiddleware from './app/middleware/auth';
 
@@ -23,6 +24,8 @@ routes.put('/users', UserController.update);
 routes.post('/files', upload.single('file'), FileController.store);
 
 routes.get('/providers', ProviderController.index);
+
+routes.get('/schedule', ScheduleController.index);
 
 routes.get('/appointments', AppointmentController.index);
 routes.post('/appointments', AppointmentController.store);
