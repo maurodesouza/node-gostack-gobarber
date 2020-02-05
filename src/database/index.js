e6f9f264d8ed3a1a472b2d44c862e22a9d49692c
Mauro De Souza <maurodesouza2017@hotmail.com>
2020-02-05 20:16:40 -0300

    fix connection mongo

--- src/database/index.js
+++ src/database/index.js
@@ -24,7 +24,7 @@ class Database {
   }
 
   mongo() {
-    this.connectionMongo = connect('mongodb://localhost:27017/gobarber', {
+    this.connectionMongo = connect('mongodb://192.168.99.100:27017/gobarber', {
       useNewUrlParser: true,
       useFindAndModify: true,
       useUnifiedTopology: true,
