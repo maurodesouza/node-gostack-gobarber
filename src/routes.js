3bd33effd58046617949d788aac3b607afa53aea
Mauro De Souza <maurodesouza2017@hotmail.com>
2020-02-04 21:39:36 -0300

    class 17 - services scheduling

--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,7 @@ import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
+import AppointmentController from './app/controllers/AppointmentController';
 
 import authMiddleware from './app/middleware/auth';
 
@@ -17,9 +18,12 @@ routes.post('/session', SessionController.store);
 
 routes.use(authMiddleware);
 
+routes.put('/users', UserController.update);
+
+routes.post('/files', upload.single('file'), FileController.store);
+
 routes.get('/providers', ProviderController.index);
 
-routes.put('/users', UserController.update);
-routes.post('/files', upload.single('file'), FileController.store);
+routes.post('/appointments', AppointmentController.store);
 
 export default routes;
