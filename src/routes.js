0e4ac6a0c0aa0453818b1e680003359a0f034992
Mauro De Souza <maurodesouza2017@hotmail.com>
2020-02-04 22:15:51 -0300

    class 19 - list user appointments

--- src/routes.js
+++ src/routes.js
@@ -24,6 +24,7 @@ routes.post('/files', upload.single('file'), FileController.store);
 
 routes.get('/providers', ProviderController.index);
 
+routes.get('/appointments', AppointmentController.index);
 routes.post('/appointments', AppointmentController.store);
 
 export default routes;
