a7c985b4453f655f99f51c3145e5ba94df9d0bf0
Mauro De Souza <maurodesouza2017@hotmail.com>
2020-02-06 22:30:53 -0300

    class 33 - exception handling

--- src/app.js
+++ src/app.js
@@ -1,18 +1,29 @@
 import express from 'express';
-import { resolve } from 'path';
-import routes from './routes';
 
+import { resolve } from 'path';
+import Youch from 'youch';
+
+import * as Sentry from '@sentry/node';
+
+import 'express-async-errors';
 import './database';
+
+import sentryConfig from './config/sentry';
+import routes from './routes';
 
 class App {
   constructor() {
     this.server = express();
 
+    Sentry.init(sentryConfig);
+
     this.middleware();
     this.routes();
+    this.exceptionHandler();
   }
 
   middleware() {
+    this.server.use(Sentry.Handlers.requestHandler());
     this.server.use(express.json());
     this.server.use(
       '/files',
@@ -22,6 +33,15 @@ class App {
 
   routes() {
     this.server.use(routes);
+    this.server.use(Sentry.Handlers.errorHandler());
+  }
+
+  exceptionHandler() {
+    this.server.use(async (err, req, res, next) => {
+      const errors = await new Youch(err, req).toJSON();
+
+      return res.status(500).json(errors);
+    });
   }
 }
 
---
+++ src/config/sentry.js
@@ -0,0 +1,3 @@
+export default {
+  dsn: 'https://d23181bacda24eacb07148ad110e504c@sentry.io/2308949',
+};
